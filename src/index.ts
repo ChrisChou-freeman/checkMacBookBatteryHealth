de2d52b3d70c91d8aca82017049482c32e40e80d
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2022-12-19 20:33:40 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import {execSync} from 'child_process';
+import {execSync} from 'child_process'
 
-const healthCount = 1000;
-const fullCycleCount = 1000/0.2;
+const healthCount = 1000
+const fullCycleCount = 1000/0.2
 
 interface BatteryInfo {
-  usedCycleCount: number;
-  remainCycleCount: number;
-  batteryHealthPercent: number;
+  usedCycleCount: number
+  remainCycleCount: number
+  batteryHealthPercent: number
 }
 
 function getHardwareInfomation(): string {
-  const GET_BATTERY_HEALTH_COMMAND = 'ioreg -l | grep Capacity | grep BatteryData';
-  const result = execSync(GET_BATTERY_HEALTH_COMMAND);
-  const stringInformation = result.toString();
+  const GET_BATTERY_HEALTH_COMMAND = 'ioreg -l | grep Capacity | grep BatteryData'
+  const result = execSync(GET_BATTERY_HEALTH_COMMAND)
+  const stringInformation = result.toString()
   return stringInformation 
 }
 
 function parserInformation(info: string): BatteryInfo {
-  const batteryInformation = info.split(' = ')[1].trim();
-  const splitInformation = batteryInformation.split(',');
-  const filterInformation = splitInformation.filter((i)=>{return i.startsWith('"CycleCount"')});
-  const usedCycleCount = parseInt(filterInformation[0].split('=')[1]);
-  const remainCycleCount = healthCount - usedCycleCount;
-  const batteryHealthPercent = Math.floor(remainCycleCount/fullCycleCount* 100) + 80;
+  const batteryInformation = info.split(' = ')[1].trim()
+  const splitInformation = batteryInformation.split(',')
+  const filterInformation = splitInformation.filter((i)=>{return i.startsWith('"CycleCount"')})
+  const usedCycleCount = parseInt(filterInformation[0].split('=')[1])
+  const remainCycleCount = healthCount - usedCycleCount
+  const batteryHealthPercent = Math.floor(remainCycleCount/fullCycleCount* 100) + 80
   return {usedCycleCount, remainCycleCount, batteryHealthPercent}
 }
 
@@ -32,8 +32,8 @@ function main(){
     used charge cycle count🔌: ${batteryInfoObj.usedCycleCount}
     remians charge cycle count: ${batteryInfoObj.remainCycleCount}
     battery health🔋: ${batteryInfoObj.batteryHealthPercent}%
-  `;
-  console.log(content);
+  `
+  console.log(content)
 }
 
-main();
+main()
